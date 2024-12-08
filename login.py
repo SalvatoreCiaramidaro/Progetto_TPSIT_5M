1b6b25676c74dbde2b5bd69a30f505c333d3d669
SalvatoreCiaramidaro <salvatore.ciaramidaro@studenti.isissgobetti.it>
2024-12-08 18:54:04 +0100

    aggiunta favicon e fix codice

--- login.py
+++ login.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template, redirect, url_for, flash, jsonify
+from flask import Flask, request, render_template, redirect, url_for, jsonify
 import mariadb
 
 app = Flask(__name__)
